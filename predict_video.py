bd2f355d4caccf12f6d972dda1281f7a133935ca
mehdigorjian <37504420+mehdigorjian@users.noreply.github.com>
2020-05-12 18:00:28 -0500

    Update predict_video.py

--- predict_video.py
+++ predict_video.py
@@ -68,8 +68,8 @@ while True:
 	output = frame.copy()
 	# output = cv2.resize(frame.copy(), (244,244))
 
-	wShape = frame.shape[1]
-	frame = frame[:wShape, :,:]
+	#wShape = frame.shape[1]
+	#frame = frame[:wShape, :,:]
 	#print(frame.shape)
 	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 	frame = cv2.resize(frame, (224, 224)).astype("float32")
